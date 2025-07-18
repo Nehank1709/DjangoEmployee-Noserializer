25476c8158a3ce8f92365ff1b652b7dc030641bd
Neha Kumari <nksinghnk0@gmail.com>
2025-07-18 19:38:30 +0530

    Removed URL conf

--- employee/urls1.py
+++ employee/urls1.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
 from employee import views
 
 urlpatterns = [
-    url("employee_list/", views.ListEmployee.as_view()),
-    url("employee_create/", views.UpsertEmployee.as_view()),
-    url("employee_delete/", views.DeleteEmployee.as_view()),
+    path("employee_list/", views.ListEmployee.as_view()),
+    path("employee_create/", views.UpsertEmployee.as_view()),
+    path("employee_delete/", views.DeleteEmployee.as_view()),
 ]
